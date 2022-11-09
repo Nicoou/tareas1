3fb71cbdb9458efee963129cfaa8db38fb6fca19
Nicoou <117428591+Nicoou@users.noreply.github.com>
2022-11-09 10:20:08 -0300

    Add files via upload

--- main.java
+++ main.java
@@ -1,24 +1,53 @@
 public class main{
 
-    public static void main(String[] args) {
-        suma(10, 10, 10);
-        Coche miCoche = new Coche();
-        miCoche.AgregarPuerta();
+    public static void main(String[] args){
+        Persona persona = new Persona();
 
-        System.out.println(miCoche.puertas);
+        persona.setEdad("edad");
 
+        persona.edad="edad";
 
+        String edad = persona.getEdad();
+        System.out.println(18);
+
+        persona.setNombre("nombre");
+
+        persona.nombre="nombre";
+
+        String nombre = persona.getNombre();
+        System.out.println("Juan");
+
+        persona.setTelefono("telefono");
+
+        persona.telefono="telefono";
+
+        String telefono = persona.getTelefono();
+        System.out.println( 12365414 );
     }
-    public static void suma(int a, int b, int c){
-        int resultado;
-        resultado = a + b + c;
-        System.out.println(resultado);
-    }
-}
+    public static class Persona {
+        private String edad;
+        private String nombre;
+        private String telefono;
 
-class Coche{
-    public int puertas = 4;
-    public void AgregarPuerta() {
-        this.puertas++;
+        public void setEdad(String edad) {
+            this.edad = edad;
+        }
+        public String getEdad(){
+            return this.edad;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+        public String getNombre(){
+            return this.nombre;
+        }
+
+        public void setTelefono(String telefono) {
+            this.telefono = telefono;
+        }
+        public String getTelefono(){
+            return this.telefono;
+        }
     }
 }
